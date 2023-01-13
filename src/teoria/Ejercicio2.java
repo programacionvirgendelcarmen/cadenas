79cc4a939958272af8e8030f78097b38e890f133
manuel <programacionvirgendelcarmen@gmail.com>
2023-01-13 15:35:38 +0100

    Ejercicio contraseña segura

--- src/teoria/Ejercicio2.java
+++ src/teoria/Ejercicio2.java
@@ -10,22 +10,65 @@ public class Ejercicio2 {
         lista.add("joaquin");
         lista.add("garcía abolafia");*/
         //.......
-        List<String> listaCSV = List.of("joaquin", "garcía abolafia");
+        List<String> listaCSV =
+                List.of("joaquin", "garcía abolafia","avda madrid 35", "953222222");
         //listaCSV.set(0, "felipe"); es inmutable
         //usamos un bucle mejorado aparezca en una línea cada uno de los valores:
         //nombre
         //apellidos
         //...
+        for (String dato : listaCSV ) {
+            System.out.println(dato);
+        }
         //Un método que pasemos la lista anterior y devuelva un String como una línea CSV
         //"nombre, apellidos, direccion, telefono" pero trabajando con StringBuilder
+        String csv1 = crearCSV(listaCSV);
         //mostramos en consola la cadena anterior, pero en mayúscula
+        System.out.printf("CSV: %S%n", csv1);
         //Pasamos la cadena anterior a un método y nos devuelva una nueva cadena
         //pero sin espacios y en mayúscula:
         //"NOMBRE,APELLIDOS,DIRECCIÓN,TELÉFONO" usando un StringBuilder
+        String csv2 = crearCSVMayusculasSinEspacios(csv1);
+        System.out.println(csv2);
         //Un método, que se le pase la cadena anterior y nos devuelva el nombre
         //con el formato apellidos, nombre usando un StringBuilder
+        String nombre = crearNombreMinuscula(csv2);
+        System.out.printf("NOMBRE: %s%n", nombre);
         //Un método que se le pasa la cadena "NOMBRE,APELLIDOS,DIRECCIÓN,TELÉFONO"
         //y un código postal devuelva "NOMBRE,APELLIDOS,DIRECCIÓN,TELÉFONO,CP"
         //usando StringBuilder
+        String csv3 = crearCSVConCP(csv2, "23008");
+        System.out.println(csv3);
+    }
+
+    private static String crearCSVConCP(String csv, String codigoPostal) {
+        StringBuilder stringBuilder = new StringBuilder(csv);
+        stringBuilder.append(',');
+        stringBuilder.append(codigoPostal);
+        return stringBuilder.toString();
+    }
+
+    private static String crearNombreMinuscula(String csv) {
+        StringBuilder stringBuilder = new StringBuilder(csv);
+        //absurdo trabaja con stringBuilder, con csv funcionaría
+        String[] tokens = stringBuilder.toString().split(",");
+        //new String[]{"NOMBRE","APELLIDOS","DIRECCION","TELEFONO"}
+        return tokens[1].toLowerCase() + ", " + tokens[0].toLowerCase();
+    }
+
+    private static String crearCSVMayusculasSinEspacios(String csv) {
+        StringBuilder stringBuilder = new StringBuilder(csv);
+        //es absurdo crear el StringBuilder porque hay que emplear
+        //el método replaceAll que es de String y csv ya es un String
+        return stringBuilder.toString().replaceAll(", ",",").toUpperCase();
+    }
+
+    private static String crearCSV(List<String> listaCSV) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String dato: listaCSV) {
+           stringBuilder.append(dato);
+           stringBuilder.append(", ");
+        }
+        return stringBuilder.substring(0, stringBuilder.length() - 2);
     }
 }
---
+++ src/teoria/Ejercicio3.java
@@ -0,0 +1,32 @@
+package teoria;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce contraseña");
+        String passwd = sc.nextLine();
+        if (passwd.length() < 8) {
+            System.out.printf("%s no es segura%n", passwd);
+            return;
+        };
+
+        boolean numero = false, simbolo = false, mayuscula = false, minuscula = false;
+        for (int i = 0; i < passwd.length(); i++) {
+            if (Character.isDigit(passwd.charAt(i)))
+                numero = true;
+            else if (Character.isLowerCase(passwd.charAt(i)))
+                minuscula = true;
+            else if (Character.isUpperCase(passwd.charAt(i)))
+                mayuscula = true;
+            else
+                simbolo = true;
+        }
+        boolean segura = numero && simbolo && mayuscula && minuscula;
+        System.out.printf("%s ¿Es segura? %B%n", passwd, segura);
+    }
+    public static boolean esContrasennaSegura(String passwd){
+        return false;
+    }
+}
